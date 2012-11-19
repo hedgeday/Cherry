703614e114206a0a6a2aae8504459d6568a3155c
hedgeday <sachin.hegde91@gmail.com>
2012-11-19 17:19:26 -0500

    restructured

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 // a very simple node server using the express module
 var express = require("express");
 var app = express();
-var wwwDir = "/";
+var wwwDir = "/www";
 app.use("/", express.static(__dirname + wwwDir));
 app.get("/", function(req, res) { res.render(wwwDir + "/index.html");});
 app.listen(8080);
---
+++ www/scripts/app.js
@@ -0,0 +1,13 @@
+var login = function(){
+	console.log(this);
+	this.setup(); 	
+};
+
+login.prototype.setup = function(){
+	setTimeout(function(){$.mobile.changePage($("#home"));}, 2000);
+	$('#submitSignIn').on('click', function(){this.validateSignIn()}.bind(this));
+	$.mobile.page.prototype.options.backBtnTheme = "d";
+	$('#notificationWarn').on('click', function(){$("#disclaimer").popup()});
+	$('#signUpButton').on('click', function(){$.mobile.changePage($("#signup"));})
+	$('#createAccButton').on('click', function(){$.mobile.changePage("homescreen.html")});
+};
