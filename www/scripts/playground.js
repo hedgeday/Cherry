b562c0b87c957002476843a55190b59f1148cbdd
domcerminara <dompkt2013@gmail.com>
2012-11-26 22:16:35 -0500

    basic functionality, need cordova + storage

--- www/scripts/playground.js
+++ www/scripts/playground.js
@@ -15,4 +15,15 @@ var Playground = function() {
     $('#addPicture').tap(function() {
         $('#newTextMessage').hide();
     });
+    
+    $('.cancelPost').tap(function() {
+        $('#newTextMessage').hide();
+    });
+    
+    $('#newTextMessagePost').tap(function() {
+        $('#newTextMessage').hide();
+        $('#messages').prepend("<div><img src='/images/profilePicF.png' />" + $('#newTextMessage input').val() + "</div>");
+        $('#newTextMessage input').val("");
+        
+    });
 };
