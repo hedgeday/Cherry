e06d56abdddb5c3fe99cc581bea549729f9f443a
domcerminara <dompkt2013@gmail.com>
2012-12-01 14:43:38 -0500

    camera starting to work wWOOOOWOWOWOOOO

--- www/scripts/playground.js
+++ www/scripts/playground.js
@@ -1,55 +1,59 @@
 var Playground = function() {
-    $('#addText').tap(function(e) {
-        $('#newTextMessage').show();
-        $('#newTextMessage input').focus();
-    });
-    
-    $('#addVideo').tap(function() {
-        $('#newTextMessage').hide();
-    });
-    
-    $('#addVoice').tap(function() {
-        $('#newTextMessage').hide();
-    });
-    
-    $('.cancelPost').tap(function() {
-        $('#newTextMessage input').val("");
-        $('#newTextMessage').hide();
-    });
-    
-    $('#newTextMessagePost').tap(function() {
-        submitPost();
-    });
 
-    $('#messageInput').bind('keypress', function(e) {
-        if (e.keyCode == 13) submitPost();
-    });
-    
-    $('#addPicture').tap(function() {
-        $('#newTextMessage').hide();
-        var destinationType = navigator.Camera.DestinationType;
-        alert(destinationType);
-        var pictureSource = navigator.camera.PictureSourceType;
-    
-        navigator.camera.getPicture(success, fail,
-            {
-                quality: 75,
-                destinationType: destinationType.DATA_URL
-            }
-         );
-        
-        function success(pic) {
-            alert(pic);
-        };
-        
-        function fail(msg) {
-            alert(msg);
-        };
-    });
+    document.addEventListener('deviceReady', function(){
 
-    function submitPost(){
-        $('#messages').append("<div class='otherMsg'>"+ $('#newTextMessage input').val() + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img class='profilePic' src='/images/profilePicM.png' /> </div>");
-        $('#messages').append("<div class='myMsg'><img class='profilePic' src='/images/profilePicF.png' />" + $('#newTextMessage input').val() + "</div>");
-        $('#messageInput').val("");
-    };
+        $('#addText').tap(function(e) {
+            $('#newTextMessage').show();
+            $('#newTextMessage input').focus();
+        });
+        
+        $('#addVideo').tap(function() {
+            $('#newTextMessage').hide();
+        });
+        
+        $('#addVoice').tap(function() {
+            $('#newTextMessage').hide();
+        });
+        
+        $('.cancelPost').tap(function() {
+            $('#newTextMessage input').val("");
+            $('#newTextMessage').hide();
+        });
+        
+        $('#newTextMessagePost').tap(function() {
+            submitPost();
+        });
+    
+        $('#messageInput').bind('keypress', function(e) {
+            if (e.keyCode == 13) submitPost();
+        });
+        
+        $('#addPicture').tap(function() {
+            $('#newTextMessage').hide();
+            var destinationType = navigator.camera.DestinationType;
+            var pictureSource = navigator.camera.PictureSourceType;
+        
+            navigator.camera.getPicture(success, fail,
+                {
+                    quality: 75,
+                    destinationType: destinationType.DATA_URL,
+                    sourceType: pictureSource
+                }
+             );
+            
+            function success(pic) {
+                alert(pic);
+            };
+            
+            function fail(msg) {
+                alert(msg);
+            };
+        });
+    
+        function submitPost(){
+            $('#messages').append("<div class='otherMsg'>"+ $('#newTextMessage input').val() + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img class='profilePic' src='/images/profilePicM.png' /> </div>");
+            $('#messages').append("<div class='myMsg'><img class='profilePic' src='/images/profilePicF.png' />" + $('#newTextMessage input').val() + "</div>");
+            $('#messageInput').val("");
+        };
+    });
 };
